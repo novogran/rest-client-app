46571e0dd2045dcb93b0268353dc7b023cfb76ca
Denis Yeresko <denis.yeresko@gmail.com>
2025-09-04 16:51:30 +0300

    fix: fix issues that occured during code review

--- src/data/developerData.ts
+++ src/data/developerData.ts
@@ -1,4 +1,4 @@
-import { DeveloperData } from '@/types/interfaces';
+import { DeveloperData } from '@/types/developerInfo.types';
 
 export const developerData: DeveloperData[] = [
   {
@@ -12,14 +12,15 @@ export const developerData: DeveloperData[] = [
     id: 2,
     name: 'Denys Yeresko',
     description:
-      "Front-End developer, working with React and TypeScript, I demonstrate integration with REST API, creation of interfaces using a clear component architecture. I'm quickly delving into the project, writing intelligent code and learning all about web.",
+      "Front-end developer. Working with React and TypeScript, I demonstrate integration with REST API, creation of interfaces using a clear component architecture. I'm quickly delving into the project, writing intelligent code and learning all about web.",
     image: '/images/denys.png',
     github: 'https://github.com/dyeresko',
   },
   {
     id: 3,
     name: 'Vitali Navahran',
-    description: '',
+    description:
+      'Team Lead. Vitali, 29, is a passionate developer who started programming in 2013 with Java, building Android apps. He later transitioned to Kotlin, embracing modern mobile development. In 2020, he improved his English to a pre-intermediate level, expanding his opportunities. Recently, he explored front-end development through RS School, learning HTML, CSS, and JavaScript, and enjoys blending creativity with problem-solving. Outside coding, he loves PC gaming, fixing electronics, car repairs, and swimming. He aims to grow as a developer, tackle bigger projects, and possibly teach in the future.',
     image: '/images/vitaliy.jpg',
     github: 'https://github.com/novogran',
   },
