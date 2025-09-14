4eda9ceb230011d55692ed785318aa034736f4c6
Vitaly <vi19954@gmail.com>
2025-09-14 03:47:37 +0300

    feat: add auth page tests

--- src/app/[locale]/auth/[[...slug]]/page.tsx
+++ src/app/[locale]/auth/[[...slug]]/page.tsx
@@ -7,7 +7,7 @@ import { notFound } from 'next/navigation';
 export default async function AuthPage({
   params,
 }: {
-  params: Promise<{ slug: string }>;
+  params: Promise<{ slug: string[] | null | undefined }>;
 }) {
   const { slug } = await params;
   const authType = slug?.[0] || 'signIn';
